f3859dc1cd7f706ca97f96c7fddc06f2e9c1ed61
Justin Gosselin <27461459+vec4@users.noreply.github.com>
2020-07-23 11:33:46 -0400

    Use ajax instead of XMLHttpRequest
    Turns out we can use ajax so long as we explicitly provide the X-Requested-With header. This is supposed to be inserted by default, but I guess PWD looks for it explicitly.

--- js/pwd-background.js
+++ js/pwd-background.js
@@ -21,7 +21,7 @@
 // SOFTWARE.
 
 /*
- * This class contains client-side functions for using PWD without having to
+ * This wrapper contains client-side functions for using PWD without having to
  * create a terminal in the browser; i.e., in the background. We use it for
  * closed-book testing in FreeCompilerCamp.
  *
@@ -32,6 +32,9 @@
  * Justin Gosselin | 07/21/2020
 */
 
+// TODO: There are many other endpoints that may be useful (see handler dir in
+// Go PWD server)
+// TODO: better error processing on HTTP requests
 'use strict';
 
 // PWD object for background only
@@ -57,31 +60,20 @@ PwdBg.prototype.newSession = function(opts, cb) {
   setOpts.call(this, opts);
 
   this.createSession(function(resp) {
-    if (resp.status == 200) {
 
-      var sessionData = JSON.parse(resp.responseText);
-
-      self.opts.baseUrl = self.opts.baseUrl.split('/')[0] + '//' + sessionData.hostname;
-      self.sessionId = sessionData.session_id; // each pwdbg instance has one session
+      self.opts.baseUrl = self.opts.baseUrl.split('/')[0] + '//' + resp.hostname;
+      self.sessionId = resp.session_id; // each pwdbg instance has one session
 
       // Create an instance for this session
       self.createInstance(opts, function(resp) {
-        if (resp.status == 200) {
-          var i = JSON.parse(resp.responseText);
+          var i = resp;
           // here we are assuming there is only ONE instance for a session
           self.instances = i; // add instance to container
 
           if (cb) {
-            cb(resp); // callback upon successful instance creation
+            cb(resp); // callback upon successful session + instance creation
           }
-        } else {
-          console.error('Could not create PWC instance.');
-        }
       });
-
-    } else {
-      console.error('Could not create PWC session.');
-    }
   });
 
 }
@@ -97,29 +89,23 @@ PwdBg.prototype.createSession = function(cb) {
   // a form submission and sends a 302 (redirect) to /p/<sessionId>, which is
   // the playground URL.
 
-  // No success using ajax - had to explicitly use XMLHttpRequest. The
-  // X-Requested-With header wasn't being passed to the server with the ajax
-  // request for whatever reason.
+  // This is supposed to be added by default with an ajax call, but it seems
+  // that PWD is explicitly looking for it.
 
-  // Request information
-  var request = new XMLHttpRequest();
-  var endpoint = this.opts.baseUrl + '/';
-  var method = 'POST';
-
-  // Data to send with the request
-  var data = encodeURIComponent('session-duration') + '=' + encodeURIComponent('90m');
-
-  request.open(method, endpoint, true); // async (last parameter)
-
-  // Set headers
-  request.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
-  request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-
-  request.onload = function() {
-    cb(request);
-  }
-
-  request.send(JSON.stringify(data));
+  $.ajax({
+    url: this.opts.baseUrl + '/',
+    data: encodeURIComponent('session-duration') + '=' + encodeURIComponent('90m'),
+    contentType: 'application/x-www-form-urlencoded',
+    headers: {'X-Requested-With': 'XMLHttpRequest'},
+    method: 'POST',
+    type: 'POST', // For jQuery < 1.9
+    success: function(r) {
+      cb(r);
+    },
+    error: function(e) {
+      console.error('Could not create PWC session.');
+    }
+  });
 }
 
 /*
@@ -130,22 +116,20 @@ PwdBg.prototype.createInstance = function(opts, cb) {
   opts.ImageName = opts.ImageName || this.opts.ImageName;
   opts.Envs = opts.InstanceEnvs || this.opts.InstanceEnvs;
 
-  var request = new XMLHttpRequest();
-  var endpoint = this.opts.baseUrl + '/sessions/' + this.sessionId + '/instances';
-  var method = 'POST';
-
-  var data = opts;
-
-  request.open(method, endpoint, true);
-
-  request.setRequestHeader('content-type', 'application/json');
-  request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-
-  request.onload = function() {
-    cb(request);
-  }
-
-  request.send(JSON.stringify(data));
+  $.ajax({
+    url: this.opts.baseUrl + '/sessions/' + this.sessionId + '/instances',
+    data: opts,
+    contentType: 'application/json',
+    headers: {'X-Requested-With': 'XMLHttpRequest'},
+    method: 'POST',
+    type: 'POST', // For jQuery < 1.9
+    success: function(r) {
+      cb(r);
+    },
+    error: function(e) {
+      console.error('Could not create PWC instance.');
+    }
+  });
 
 }
 
@@ -158,20 +142,19 @@ PwdBg.prototype.createInstance = function(opts, cb) {
 PwdBg.prototype.closeSession = function(cb) {
 
   if (this.sessionId) {
-    var request = new XMLHttpRequest();
-    var endpoint = this.opts.baseUrl + '/sessions/' + this.sessionId;
-    var method = 'DELETE';
-
-    request.open(method, endpoint, true);
-
-    request.setRequestHeader('content-type', 'application/json');
-    request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-
-    request.onload = function() {
-      cb(request);
-    }
-
-    request.send(JSON.stringify(data));
+    $.ajax({
+      url: this.opts.baseUrl + '/sessions/' + this.sessionId,
+      contentType: 'application/application/json',
+      headers: {'X-Requested-With': 'XMLHttpRequest'},
+      method: 'DELETE',
+      type: 'DELETE', // For jQuery < 1.9
+      success: function(r) {
+        cb(r);
+      },
+      error: function(e) {
+        console.error('Could not close PWC session.');
+      }
+    });
   }
 
 }
@@ -183,25 +166,6 @@ PwdBg.prototype.closeSession = function(cb) {
 */
 PwdBg.prototype.upload = function(name, path, data, cb) {
 
-  /*
-  var request = new XMLHttpRequest();
-  var endpoint = this.opts.baseUrl + '/sessions/' + this.sessionId + '/instances/'
-                    + name + '/uploads?path=' + path;
-  var method = 'POST';
-
-  request.open(method, endpoint, true);
-
-  request.setRequestHeader('content-type', 'multipart/form-data;');
-  request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-
-  request.onload = function() {
-    cb(request);
-  }
-
-  request.send(JSON.stringify(data));
-  */
-
-  // For some reason, only AJAX worked here....
   $.ajax({
       url: this.opts.baseUrl + '/sessions/' + this.sessionId + '/instances/'
             + name + '/uploads?path=' + path,
@@ -210,9 +174,9 @@ PwdBg.prototype.upload = function(name, path, data, cb) {
       contentType: false,
       processData: false,
       method: 'POST',
-      type: 'POST', // For jQuery < 1.9
+      type: 'POST',
       success: function(data) {
-        console.log("Successfully uploaded file to instance.");  
+        console.log("Successfully uploaded file to instance.");
         cb(data);
       }
   });
@@ -225,7 +189,7 @@ PwdBg.prototype.upload = function(name, path, data, cb) {
 function setOpts(opts) {
   var opts = opts || {};
   this.opts = opts;
-  this.opts.baseUrl = this.opts.baseUrl || 'https://lab.freecompilercamp.org';
+  this.opts.baseUrl = this.opts.baseUrl || '{{ site.pwdurl }}';
   this.opts.ports = this.opts.ports || [];
   this.opts.ImageName = this.opts.ImageName || '';
   this.opts.InstanceEnvs = this.opts.InstanceEnvs || [];
